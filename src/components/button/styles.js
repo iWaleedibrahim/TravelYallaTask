86561894b9794502d0a1f788fb15a2b95bb09855
Waleed Ibrahim <iwaleedibrahim@gmail.com>
2021-02-10 05:11:50 +0200

    ..

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -7,12 +7,13 @@ import COLORS from "../../common/colors"
 const styles = StyleSheet.create({
   container: {
     height: DIMS.screenWidth / 7.8125,
-    width: DIMS.screenWidth * 0.872,
+    width: DIMS.screenWidth * 0.7493,
     backgroundColor: COLORS.TRAVELYALLA_BLUE,
     borderRadius: 8,
     alignItems: "center",
     justifyContent: "center",
     margin: 10,
+    alignSelf: 'center'
   },
   title: {
     color: COLORS.white,
